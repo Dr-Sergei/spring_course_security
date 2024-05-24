bfe912fa7c05b1e232202c5f668e0ea8294046e5
Wittchen <swittche@ipb-halle.de>
2024-05-24 10:29:18 +0200

    phase 2: added MyConfig file -> routing for jsp

---
+++ src/main/java/com/sergei/spring/security/configuration/MyConfig.java
@@ -0,0 +1,23 @@
+package com.sergei.spring.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = "com.sergei.spring.security")
+@EnableWebMvc
+public class MyConfig {
+
+    //adding routing for JSP in analogue to applicationContext.xml
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/WEB-INF/view/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+}
