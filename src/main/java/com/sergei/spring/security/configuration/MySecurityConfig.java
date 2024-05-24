ac40941976bcc1e74d2dedae17784a8bf891eda6
Wittchen <swittche@ipb-halle.de>
2024-05-24 15:31:51 +0200

    phase 8: taglib dependency added, as well as security authorize

--- src/main/java/com/sergei/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/sergei/spring/security/configuration/MySecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -34,7 +35,7 @@ public class MySecurityConfig {
 
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+    public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withDefaultPasswordEncoder()
                 .username("sergei")
                 .password("aaa")
--- src/main/webapp/WEB-INF/view/viewForAllEmployees.jsp
+++ src/main/webapp/WEB-INF/view/viewForAllEmployees.jsp
@@ -1,15 +1,20 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <body>
 
 <h3>Information for all employees</h3>
 <br/>
+
+<security:authorize access="hasRole('HR')">
 <input type="button" value="Salary" onclick="window.location.href ='hrInfo'">
 Only for HR Stuff
+</security:authorize>
 <br/>
-
+<security:authorize access="hasRole('MANAGER')">
 <input type="button" value="Performance" onclick="window.location.href ='managerInfo'">
 Only for Managers
+</security:authorize>
 <br/>
 
 </body>
